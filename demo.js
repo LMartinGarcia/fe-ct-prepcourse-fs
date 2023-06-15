55234a9ad43579df22e0db0faf4ae21a87812940
LMartinGarcia <lucianno.g@hotmail.com>
2023-06-14 21:17:25 -0300

    finalizado

---
+++ demo.js
@@ -0,0 +1,36 @@
+var nombre = "leo";
+
+
+
+
+// LOGICOS
+
+var resultadoLogico = (100 > 200)&&( 3 >= 3 );
+
+// metodos
+
+var nombre = 'Alejo';
+
+// funciones
+
+function sumar(a, b) {
+    var suma = a + b;
+    return suma;
+}
+
+// condicionales
+var edad = 19;
+
+if(edad > 18) {
+    console.log('soy mayor de edad');
+} else {
+    console.log('soy menor de edad');
+}
+
+function dominioDelPais(dominio) {
+    if(dominio=== "co"){
+        console.log('el pais es colombia');
+    }   else if (dominio === "br"){
+        console.log('el pais es Brasil');
+    }
+}
